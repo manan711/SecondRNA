860cef8fc338554b4726cd27a486a8e4e4b8a9b5
Manan711 <mananpatel.711@gmail.com>
2023-05-17 15:00:41 -0400

    initial commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import {SafeAreaView, ScrollView, Text} from 'react-native';
+import React from 'react';
+import FlatCards from './components/FlatCards';
+import ElevatedCards from './components/ElevatedCards';
+import FancyCard from './components/FancyCard';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <FlatCards />
+        <ElevatedCards />
+        <FancyCard />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ components/ElevatedCards.tsx
@@ -0,0 +1,63 @@
+/* eslint-disable prettier/prettier */
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function ElevatedCards() {
+  return (
+    <View>
+      <Text style={styles.headingText}>Elevated Cards</Text>
+      <ScrollView horizontal={true} style={styles.container}>
+        <View style={[styles.card, styles.cardElevated]}>
+            <Text>Tap</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+            <Text>me</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+            <Text>to</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+            <Text>Scroll</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+            <Text>more...</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+            <Text>😁</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    headingText: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        paddingHorizontal: 10,
+    },
+    container: {
+        padding: 10,
+    },
+    card: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 100,
+        height: 100,
+        borderRadius: 5,
+        margin: 10,
+        borderColor: '#5DA3FA',
+        borderWidth: 1,
+    },
+    cardElevated: {
+        backgroundColor: '#CAD5E2',
+        elevation: 10,
+        shadowOffset: {
+            width: 10,
+            height: 10,
+        },
+        shadowColor: 'blue',
+        shadowRadius: 5,
+    },
+});
